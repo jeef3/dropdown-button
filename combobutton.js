29156df3f02ea0cba510e4a9a30bba8ed7878d33
Jeff K <mail@jeef3.com>
2011-12-22 13:47:04 +1300

    Basic functionality complete

--- combobutton.js
+++ combobutton.js
@@ -1,8 +1,8 @@
 (function($) {
     function ComboButton(element, options) {
         var _this = this,
-            $element = $(element),
-            $options = $element.next();
+            $button = $(element),
+            $options = $button.next();
         
         var defaults = {
             options: {}
@@ -10,28 +10,41 @@
         var settings = $.extend({}, defaults, options);
         
         var construct = function() {
-            $element.on("click", _clicked);
+            $button.on("click", _buttonClicked);
             $options.on("click", "", _optionClicked);
+            $(document).on("click", _documentClicked);
         };
-        
+
+        // Public Methods
         this.show = function() {
-            
+            $button.addClass("options-visible");
         };
         
         this.hide = function() {
-            
+            $button.removeClass("options-visible");
         };
-        
-        var _selected = function() {
-            
-        };
-        
-        var _clicked = function(e) {
-            
+
+        // Private Methods
+        var _buttonClicked = function(e) {
+            $button.toggleClass("options-visible", !$button.hasClass("options-visible"));
         };
         
         var _optionClicked = function(e) {
-            
+            var item = $(this),
+                action =  item.data("combo-button-action") ||
+                    item.attr("id") ||
+                    item.text().replace(/ /g, "-");
+
+            $button.trigger("click:" + action);
+            _this.hide();
+        };
+
+        var _documentClicked = function(e) {
+            if (!$button.hasClass("options-visible") ||
+                    e.target === element ||
+                    $.contains($options[0], e.target)) return;
+
+            _this.hide();
         };
         
         construct();
--- test/spec/combobutton_spec.js
+++ test/spec/combobutton_spec.js
@@ -5,14 +5,58 @@ describe("Combo Button", function() {
     
     beforeEach(function() {
         loadFixtures("fixture.html");
-        combobutton = new ComboButton();
+        button = $("#button");
+        options = button.next();
     });
-    
-    it("should show the options on 'show'", function() {
-        //combobutton.
+
+    describe("Show/Hide", function() {
+
+        beforeEach(function() {
+            combobutton = new ComboButton(button[0]);
+        });
+
+        it("should show the options on 'show'", function() {
+            combobutton.show();
+            expect(button.hasClass("options-visible")).toBeTruthy();
+        });
+
+        it("should hide the options on 'hide'", function() {
+            combobutton.hide();
+            expect(button.hasClass("options-visible")).toBeFalsy();
+        });
+
+        it("should show the options when the user clicks the button", function() {
+            button.click();
+            expect(button.hasClass("options-visible")).toBeTruthy();
+        });
+
+        it("should hide when the user clicks an option", function() {
+            button.click();
+            options.first().click();
+            expect(button.hasClass("options-visible")).toBeFalsy();
+        });
+
+        it("should hide when the user clicks the button again", function() {
+            button.click().click();
+            expect(button.hasClass("options-visible")).toBeFalsy();
+        });
+
+        it("should hide when the user clicks outside the button or options", function() {
+            button.click();
+            $("body").click();
+            expect(button.hasClass("options-visible")).toBeFalsy();
+        });
     });
-    
-    it("should hide the options on 'hide'", function() {
-        
+
+    describe("jQuery Plugin", function() {
+
+        it("should have a jQuery plugin", function() {
+            expect(button.combobutton).toBeDefined();
+        });
+
+        it("should store a reference to itself on the button data", function() {
+            var ref = button.combobutton().data("combobutton");
+            expect(ref instanceof ComboButton).toBeTruthy();
+        });
     });
 });
